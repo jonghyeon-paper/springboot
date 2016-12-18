af08c98a93b90ba7a1a71643dcbf1e9809ebc900
good <good@good-pc>
2016-12-19 02:56:43 +0900

    pageable

--- src/main/java/kr/co/springboot/persistence/hibernate/UserRepository.java
+++ src/main/java/kr/co/springboot/persistence/hibernate/UserRepository.java
@@ -1,9 +1,17 @@
 package kr.co.springboot.persistence.hibernate;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import kr.co.springboot.model.hibernate.User;
 
-public interface UserRepository extends CrudRepository<User, String> {
-
+public interface UserRepository extends PagingAndSortingRepository<User, String> {
+	
+	List<User> findByUsernameContainingOrderByUsername(String name);
+	
+	Page<User> findByUsernameContainingOrderByUsername(String name, Pageable pageable);
+	
 }
--- src/main/java/kr/co/springboot/service/impl/hibernate/HelloServiceImpl.java
+++ src/main/java/kr/co/springboot/service/impl/hibernate/HelloServiceImpl.java
@@ -1,9 +1,13 @@
 package kr.co.springboot.service.impl.hibernate;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,21 +28,26 @@ public class HelloServiceImpl implements HelloService {
 	@Override
 	public List<User> getUserList() {
 //		List<Member> userList = hibernateTemplete.loadAll(Member.class);
-		List<User> memberList = (List<User>) userRepository.findAll();
-		System.out.println("memberList > " + memberList.size());
-		for (User member : memberList) {
-			System.out.println(member.toString());
-		}
+//		List<User> memberList = (List<User>) userRepository.findByUsernameContainingOrderByUsername("AAA");
+//		System.out.println("memberList > " + memberList.size());
+//		for (User member : memberList) {
+//			System.out.println(member.toString());
+//		}
 		
-		return memberList;
+		Page<User> memberList = (Page<User>) userRepository.findByUsernameContainingOrderByUsername("AAA", new PageRequest(0, 7, new Sort(Sort.Direction.ASC, "username")));
+		System.out.println(memberList.toString());
+		
+		return memberList.getContent();
 	}
 
 	@Override
 	@Transactional
 	public Map<String, Object> addUser(User member) {
-		
-		member = new User("AAA", "BBB", true);
-		userRepository.save(member);
+		DecimalFormat df = new DecimalFormat("00");
+		for (int i=0; i<20; i++) {
+			member = new User("AAA" + df.format(i), "BBB" + df.format(i), true);
+			userRepository.save(member);
+		}
 		return null;
 	}
 
